a43f4df1d1c40cd8bd79ca22008ed72683e01e04
Ana Luiza Pereira Sampaio <52867124+analuiza-dev@users.noreply.github.com>
2021-01-19 10:43:47 -0300

    upload de arquivos

---
+++ spring-java/src/main/java/com/example/springjava/controllers/ClientesController.java
@@ -0,0 +1,24 @@
+package com.example.springjava.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springjava.models.Cliente;
+/**
+ * 
+ * @author Luiza Sampaio
+ *
+ */
+@RestController
+public class ClientesController {
+	
+	@GetMapping(path = "/clientes/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(1, "Luiza", "222.333.111-01");
+	}
+	
+	@GetMapping(path = "/")
+	public String ola() {
+		return "oi";
+	}
+}
