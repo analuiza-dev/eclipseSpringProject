58bb6fd7bc8c3b53a4e82cc0de8b52889bf1f0f6
Ana Luiza <analupsampaio@gmail.com>
2021-01-22 12:12:43 -0300

    Adicionando comentario em MetodosHttpController

--- spring-java/src/main/java/com/example/springjava/controllers/MetodosHttpController.java
+++ spring-java/src/main/java/com/example/springjava/controllers/MetodosHttpController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 
+ * @author Luiza Sampaio
+ *
+ */
 @RestController
 @RequestMapping("/metodos")
 public class MetodosHttpController {
